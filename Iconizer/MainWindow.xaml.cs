929c5a29be1a6eb2818da79d727a167413afdae7
Anthony ramos roncal <codeartprogrammer@gmail.com>
2025-04-27 12:37:15 -0500

    refactor : clean imports

--- Iconizer/MainWindow.xaml.cs
+++ Iconizer/MainWindow.xaml.cs
@@ -1,20 +1,9 @@
-﻿using System;
-using System.Diagnostics;
-using System.Text;
+﻿using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.IO;
 using Iconizer.View.UserControls;
 using System.Text.Json;
 using Path = System.IO.Path;
-using Microsoft.Win32;
 using System.Runtime.InteropServices;
 
 namespace Iconizer;
