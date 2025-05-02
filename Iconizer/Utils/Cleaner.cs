5fd87a4f3ed6ce7a094e16044c7fe54db808ad07
Anthony ramos roncal <codeartprogrammer@gmail.com>
2025-05-01 19:27:40 -0500

    feat: Create a cleaner for future testing purposes

---
+++ Iconizer/Utils/Cleaner.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+
+namespace Iconizer.Utils;
+
+public class Cleaner
+{
+    public static void Clean(string[] folders)
+    {
+        foreach (var folder in folders)
+        {
+            string [] dataFiles = Directory.GetFiles(folder, "*.*", SearchOption.TopDirectoryOnly);
+            foreach (var file in dataFiles)
+            {
+                switch (Path.GetFileName(file))
+                {
+                    case "desktop.ini":
+                    case "iconizer.ico":
+                        File.Delete(file);
+                        break;
+                }
+                
+            }
+            
+        }
+    }
+    
+}
