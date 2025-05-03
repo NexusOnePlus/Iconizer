2378562060690d97ef19cbd7527e7fdfd9d9b964
William <wv35788@gmail.com>
2025-05-03 18:17:56 -0500

    feat: System Tray and Icon App IC-5

--- Iconizer/App.xaml.cs
+++ Iconizer/App.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using Iconizer.Infrastructure;
+using Iconizer.Presentation;
+
 
 namespace Iconizer;
 
@@ -9,5 +12,48 @@ namespace Iconizer;
 /// </summary>
 public partial class App : System.Windows.Application
 {
+    public static MainWindow? MainInstance { get; private set; }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        Background.BackgroundApp.Initialize();
+
+        MainWindow window = new Presentation.MainWindow();
+        MainInstance = window;
+        MainInstance.Closing += MainIsClosing;
+        MainInstance.Closed += (_, _) => { MainInstance = null; };
+        // window.Show();
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        Background.BackgroundApp.Dispose();
+        base.OnExit(e);
+    }
+
+    public static void ShowWindow()
+    {
+        Current.Dispatcher.Invoke(() =>
+        {
+            if (MainInstance == null)
+            {
+                MainInstance = new MainWindow();
+                System.Windows.Application.Current.MainWindow = MainInstance;
+                MainInstance.Closing += ((App)Current).MainIsClosing;
+                MainInstance.Closed += (_, _) => { MainInstance = null; };
+            }
+
+            MainInstance.Show();
+            MainInstance.Activate();
+            MainInstance.Focus();
+        });
+    }
+
     
+    private void MainIsClosing(object? sender, System.ComponentModel.CancelEventArgs e)
+    {
+            e.Cancel = true;
+            MainInstance!.Hide();
+    }
 }
--- Iconizer/Infrastructure/Background.cs
+++ Iconizer/Infrastructure/Background.cs
@@ -1,7 +1,91 @@
+using Hardcodet.Wpf.TaskbarNotification;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows;
+using System.Reflection;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using Iconizer.Presentation;
 
 namespace Iconizer.Infrastructure;
 
 public class Background
 {
-    
+    public static class BackgroundApp
+    {
+        private static TaskbarIcon? TrayIcon;
+
+        public static void Initialize()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            using Stream? stream = assembly.GetManifestResourceStream("Iconizer.Assets.extension_icon.ico");
+            if (stream == null)
+            {
+                string iconPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets", "extension_icon.ico");
+                if (File.Exists(iconPath))
+                {
+                    TrayIcon = new TaskbarIcon
+                    {
+                        Icon = new Icon(iconPath),
+                        ToolTipText = "Iconizer Running"
+                    };
+                }
+                else
+                {
+                    MessageBox.Show(
+                        "Icon Not Found");
+                    TrayIcon = new TaskbarIcon
+                    {
+                        ToolTipText = "Iconizer"
+                    };
+                }
+            }
+            else
+            {
+                TrayIcon = new TaskbarIcon
+                {
+                    Icon = new System.Drawing.Icon(stream),
+                    ToolTipText = "Iconizer"
+                };
+            }
+
+            TrayIcon.ContextMenu = CreateContextMenu();
+            TrayIcon.TrayContextMenuOpen += (s, e) =>
+            {
+                if (TrayIcon.ContextMenu != null)
+                {
+                    TrayIcon.ContextMenu.Dispatcher.BeginInvoke(DispatcherPriority.ApplicationIdle, new Action(() => {
+                        if (TrayIcon.ContextMenu.IsOpen)
+                        {
+                            TrayIcon.ContextMenu.Focus();
+                        }
+                    }));
+                }
+            };
+
+            TrayIcon.TrayLeftMouseDown += (_, _) => App.ShowWindow();
+        }
+
+        private static ContextMenu CreateContextMenu()
+        {
+            var menu = new ContextMenu();
+            var openItem = new MenuItem { Header = "Open" };
+            openItem.Click += (_, _) => App.ShowWindow();
+            menu.Items.Add(openItem);
+
+            menu.Items.Add(new Separator());
+            
+            var exitItem = new MenuItem { Header = "Exit" };
+            exitItem.Click += (_, _) => System.Windows.Application.Current.Shutdown();
+            menu.Items.Add(exitItem);
+
+            return menu;
+        }
+
+        public static void Dispose()
+        {
+            TrayIcon?.Dispose();
+        }
+    }
 }
